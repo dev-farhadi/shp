6d1bc798e8298d70871429bade789f424a1de832
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-27 14:12:09 +0330

    create Object Type and Category models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Type_Object, Category_Object, Object
+
+admin.site.register(Object)
+admin.site.register(Type_Object)
+admin.site.register(Category_Object)
 
 # Register your models here.
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+class Type_Object(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.type)
+
+class Category_Object(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    category = models.CharField(max_length=255)    
+
+    def __str__(self):
+        return str(self.category)
+
+class Object(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=1080)
+    category = models.ForeignKey(Category_Object, on_delete=models.CASCADE, null=True)
+    type = models.ForeignKey(Type_Object, on_delete=models.CASCADE, null=True)
+
+
+
 # Create your models here.
