2f0766e2c3ff407f556ee79a1d4a85c42986bc25
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-28 12:09:11 +0330

    create home page with paginator and show objects

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from .models import Object
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    object = Object.objects.order_by('-id')[:5]
+    paginator = Paginator(object, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'home.html' ,{'page_obj' : page_obj})
