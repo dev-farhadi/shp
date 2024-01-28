5ee2abe0ef97c3eda7e117594e0ae6a7e3733403
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-28 14:41:48 +0330

    add child_obj for sort by type of object

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('objects/<str:param>/', views.child_obj, name="child"),
+    path('object/<int:oi>', views.object, name="object"),
     path('', views.home, name="home")
 ]
--- shop/views.py
+++ shop/views.py
@@ -8,3 +8,10 @@ def home(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'home.html' ,{'page_obj' : page_obj})
+def object(request,oi):
+    object = Object.objects.filter(id=oi)
+    return render(request, 'object.html', {'object' : object})
+
+def child_obj(request,param):
+    object = Object.objects.filter(type__type=param)
+    return render(request,'child_obj.html', {'object' : object})
