bb6d8bcc5ecfeb3bf75bdce004c6498fadfd7288
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-29 10:36:15 +0330

    shopping cart createt but still work on it

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
-from .models import Object
+from .models import Object, Object_User
 from django.contrib.auth.decorators import login_required
+from .forms import AddToCartForm
 # Create your views here.
 def home(request):
     object = Object.objects.order_by('-id')[:5]
@@ -22,5 +23,12 @@ def child_obj(request,param):
 
 
 @login_required
-def shoping(request,obj_id):
-    return render(request, 'shoping.html')
+def shoping(request, obj_id):
+    shop_item = Object_User.objects.filter(user_id=request.user.id)
+    if request.method == 'POST':
+        form = AddToCartForm(request.POST)
+        if form.is_valid():
+            quantity = form.cleaned_data['quantity']
+            userobject = Object_User(object_id=obj_id, quantity=quantity, user_id=request.user.id)
+            userobject.save()
+        return render(request, 'shoping.html', {'shop_item': shop_item})
