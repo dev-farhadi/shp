92fd3123b7996c90c008648070bd705485509087
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-02-04 15:06:14 +0330

    remove useless imports!

--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from shop.models import Product
 from django.http import JsonResponse
-from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from .models import Product, Order
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from .models import Product
+
 # Create your views here.
 def home(request):
     object = Product.objects.order_by('-id')[:5]
